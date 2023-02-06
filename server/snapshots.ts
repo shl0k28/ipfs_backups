c92f02c4e74b88bdeadc1286e2326fedcd1cfadf
Shlok D. Mange <shlok_mange@protonmail.com>
2023-02-06 10:44:49 +0530

    fetch data dynamo db

--- server/snapshots.ts
+++ server/snapshots.ts
@@ -1,5 +1,6 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
+import { uploadBuffer } from '@lighthouse-web3/sdk'
 
 class Ipfs_Backups {
     private dynamoClient: DynamoDBDocument
@@ -13,5 +14,12 @@ class Ipfs_Backups {
             region
         })
         this.dynamoClient = DynamoDBDocument.from(dynamoDbClient)
+    }
+
+    async backupAwsDynamo(tableName: string, token: string, fileName: string) {
+        let dynamo = this.dynamoClient
+        const res = await dynamo.scan({ TableName: tableName })
+        let items = res.Items
+        let curr_date = new Date()
     }
 }
